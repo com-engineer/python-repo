8c4e0005d335361d97af78925865ddc781d92795
com-engineer <gaurav0207vesit@gmail.com>
2026-02-24 17:04:17 +0530

    comand line execution usingf python cell

--- 01_Basics/hello_chai.py
+++ 01_Basics/hello_chai.py
@@ -9,3 +9,8 @@ chai(4);
 one="Good Morning"
 two="Good evening"
 three="Good night"
+# reloading inside the python cell
+# from importlib import reload 
+# >>> reload(hello_chai)
+# we can direcyly import the folder 
+# import hello_chai 
