89d34090b78f53374fba48b1dcbaa7221fef83d4
com-engineer <gaurav0207vesit@gmail.com>
2026-06-17 21:43:23 +0530

    new type of code with related to list comprehension is added

--- 01_Basics/list.py
+++ 01_Basics/list.py
@@ -43,5 +43,20 @@ if "Pune" in states:
 # states_copy=states-->it give the reference of the states do not give the copy
 # states_copy=states.copy()--.have different reference
 # states_copy.append("Karnataka")
+
 squared_nums=[x**2 for x in range(10)]#list comprehension
 print(squared_nums)
+
+# another example of the list comprehension
+#  x = int(input())
+#     y = int(input())
+#     z = int(input())
+#     n = int(input())
+    
+#     result = [[i,j,k] 
+#                 for i in range(x+1)
+#                 for j in range(y+1)
+#                 for k in range(z+1)
+#                 if i+j+k != n]   
+                
+#     print(result)
